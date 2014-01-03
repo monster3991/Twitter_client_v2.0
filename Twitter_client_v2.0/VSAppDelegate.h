96e28d9c64317964925dd24d430aed061f74287b
monster3991 <monster3991@gmail.com>
2014-01-03 12:46:17 +0400

    Initial Commit

---
+++ Twitter_client_v2.0/VSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  VSAppDelegate.h
+//  Twitter_client_v2.0
+//
+//  Created by Admin on 03.01.14.
+//  Copyright (c) 2014 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Twitter_client_v2.0Tests/Twitter_client_v2_0Tests.m
@@ -0,0 +1,34 @@
+//
+//  Twitter_client_v2_0Tests.m
+//  Twitter_client_v2.0Tests
+//
+//  Created by Admin on 03.01.14.
+//  Copyright (c) 2014 Admin. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Twitter_client_v2_0Tests : XCTestCase
+
+@end
+
+@implementation Twitter_client_v2_0Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
