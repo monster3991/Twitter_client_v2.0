ccc2608c6e7e49da0b76d362cb7c7b8e398db095
monster3991 <monster3991@gmail.com>
2014-01-07 14:19:25 +0400

    with images

---
+++ Twitter_client_v2.0/Twitter_client_v2.0/Twitter_client_v2.0/Twitter_client_v2.0/Twitter_client_v2.0/Twitter_client_v2.0/Twitter_client_v2.0/Twitter_client_v2.0/Twitter_client_v2.0/Twitter_client_v2.0/Twitter_client_v2.0/Twitter_client_v2.0/Twitter_client_v2.0/Twitter_client_v2.0/Twitter_client_v2.0/Twitter_client_v2.0/Twitter_client_v2.0/Twitter_client_v2.0/Twitter_client_v2.0/Twitter_client_v2.0/Twitter_client_v2.0/Twitter_client_v2.0/Twitter_client_v2.0/Twitter_client_v2.0/Twitter_client_v2.0/Twitter_client_v2.0/Twitter_client_v2.0/Twitter_client_v2.0/Twitter_client_v2.0/Twitter_client_v2.0/Twitter_client_v2.0/Twitter_client_v2.0/Twitter_client_v2.0/Twitter_client_v2.0/Twitter_client_v2.0/Twitter_client_v2.0/Twitter_client_v2.0/Twitter_client_v2.0/Twitter_client_v2.0/Twitter_client_v2.0/Twitter_client_v2.0/Twitter_client_v2.0/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Twitter_client_v2.0
+//
+//  Created by Admin on 03.01.14.
+//  Copyright (c) 2014 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "VSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([VSAppDelegate class]));
+    }
+}
